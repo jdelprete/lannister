9f015b916b8202193659838cf9e5b663254b2919
Maurice Zhang <mauricezhang2@gmail.com>
2017-10-02 22:50:59 -0400

    Small view fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_layout_variables
-  before_action :authenticate_user!, :config_shopify_gem, unless: :devise_controller?
+  before_action :authenticate_user!, :config_shopify_gem, :set_layout_variables, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def set_layout_variables
     @num_unfulfilled_orders = current_user.aliexpress_orders.where(tracking_code: nil).group(:order_id).count.length
+    @num_unimported_products = current_user.products.where(shopify_id: nil).size
   end
 
   def config_shopify_gem
