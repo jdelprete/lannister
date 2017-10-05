feec532e3ee2ce3eb05fc21a8141a49d5c7c67a7
Maurice Zhang <mauricezhang2@gmail.com>
2017-10-04 22:50:28 -0400

    Fix processing orders query

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
     if params[:status] == 'to_order'
       @orders = current_user.orders.distinct.joins(:aliexpress_orders).where(:aliexpress_orders => { ali_order_number: nil }).order('shopify_order_number DESC')
     elsif params[:status] == 'processing'
-      @orders = current_user.orders.distinct.joins(:aliexpress_orders).where(:aliexpress_orders => { tracking_code: nil }).order('shopify_order_number DESC')
+      @orders = current_user.orders.distinct.joins(:aliexpress_orders).where(:aliexpress_orders => { tracking_code: nil }).where.not(:aliexpress_orders => { ali_order_number: nil }).order('shopify_order_number DESC')
     else
       @orders = current_user.orders.order('shopify_order_number DESC')
     end
