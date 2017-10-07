a717d1c18f32d3104262aa9f353cbe1a345c97ca
Maurice Zhang <mauricezhang2@gmail.com>
2017-10-07 12:16:01 -0400

    Implement dropdown to select a range of dates for the chart

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,10 +3,14 @@
 
 $(document).on('turbolinks:load', function() {
   var today = new Date();
-  var sevenDaysAgo = new Date(today.getTime() - 604800000); // 604800000 ms == 7 days
+  var sevenDaysAgo = new Date(today.getTime() - daysToMillis(7));
 
   var $dateFromPicker = $('.date-from');
   var $dateToPicker = $('.date-to');
+  var $chartDropdown = $('.chart-dropdown');
+
+  var dateFromDropdown = false; // used to prevent datepicker change from sending ajax request when dropdown is used
+  var dateToDropdown = false; // used to prevent datepicker change from sending ajax request when dropdown is used
 
   $dateFromPicker.datepicker({
     autoHide: true,
@@ -14,6 +18,11 @@ $(document).on('turbolinks:load', function() {
     date: sevenDaysAgo,
     autoPick: true
   }).on('pick.datepicker', function(e) {
+    if (dateFromDropdown) {
+      dateFromDropdown = false;
+      return;
+    }
+
     var dateFrom = e.date;
     var dateTo = $dateToPicker.datepicker('getDate');
 
@@ -23,6 +32,7 @@ $(document).on('turbolinks:load', function() {
       return;
     }
 
+    $chartDropdown.val('custom').change();
     getChartData(dateFrom, dateTo);
   });
 
@@ -31,6 +41,11 @@ $(document).on('turbolinks:load', function() {
     format: 'yyyy-mm-dd',
     autoPick: true
   }).on('pick.datepicker', function(e) {
+    if (dateToDropdown) {
+      dateToDropdown = false;
+      return;
+    }
+
     var dateTo = e.date;
     var dateFrom = $dateFromPicker.datepicker('getDate');
 
@@ -40,10 +55,61 @@ $(document).on('turbolinks:load', function() {
       return;
     }
 
+    $chartDropdown.val('custom').change();
     getChartData(dateFrom, dateTo);
   });
 
   $dateFromPicker.add($dateToPicker).on('keydown', function(e) { e.preventDefault(); });
+
+  $('.chart-dropdown').on('change', function() {
+    var dateRange = $(this).val();
+    var today = new Date();
+    var dateFrom = new Date();
+    var dateTo = new Date();
+
+    switch (dateRange) {
+      case 'today':
+        break;
+      case 'yesterday':
+        dateFrom = new Date(today.getTime() - daysToMillis(1));
+        dateTo = dateFrom;
+        break;
+      case 'last_7':
+        dateFrom = new Date(today.getTime() - daysToMillis(7));
+        break;
+      case 'last_30':
+        dateFrom = new Date(today.getTime() - daysToMillis(30));
+        break;
+      case 'last_90':
+        dateFrom = new Date(today.getTime() - daysToMillis(90));
+        break;
+      case 'week_to_date':
+        dateFrom = new Date(today.getTime() - daysToMillis(today.getDay()));
+        break;
+      case 'month_to_date':
+        dateFrom.setDate(1);
+        break;
+      case 'quarter_to_date':
+        dateFrom.setMonth((Math.floor((today.getMonth() + 3) / 3) - 1) * 3);
+        dateFrom.setDate(1);
+        break;
+      case 'year_to_date':
+        dateFrom.setMonth(0);
+        dateFrom.setDate(1);
+        break;
+      default:
+        return;
+        break;
+    }
+
+    dateFromDropdown = true;
+    dateToDropdown = true;
+
+    $dateFromPicker.datepicker('setDate', dateFrom);
+    $dateToPicker.datepicker('setDate', dateTo);
+
+    getChartData(dateFrom, dateTo);
+  });
 });
 
 function getChartData(dateFrom, dateTo) {
@@ -62,4 +128,8 @@ function getChartData(dateFrom, dateTo) {
     $('.dash-metric-costs').text(data.total_cost.toFixed(2));
     $('.dash-metric-earnings').text((data.total_sales - data.total_cost).toFixed(2));
   });
+}
+
+function daysToMillis(numDays) {
+  return numDays * 24 * 60 * 60 * 1000;
 }
